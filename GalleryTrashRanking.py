f747ae82040ef4756808442aa7ab4b6a45bf91d7
Yongseok Jin <mnm102211@gmail.com>
2019-07-05 17:52:56 +0900

    temporarily saved

--- GalleryTrashRanking.py
+++ GalleryTrashRanking.py
@@ -12,6 +12,8 @@ import os
 import threading
 from threading import Thread
 
+NUM_THREAD = 16
+
 #
 # thread that has return value.
 # 
@@ -68,7 +70,7 @@ def check_gallery_id (gallery_id):
 # get the gallery information from user.
 # @return(str, int, int)		gallery id, start number, end number
 #
-def get_gallery_info():
+def get_gallery_info ():
 	gallery_id = input ("갤러리 ID를 입력하세요(ex: pumpitup): ")
 	while (check_gallery_id (gallery_id) == False):
 		print ("잘못된 ID입니다.")
@@ -80,9 +82,45 @@ def get_gallery_info():
 	return gallery_id, start_num, end_num
 
 #
+# per-thread function of collect_gallery_data.
+# @param gallery_id(str)			id of gallery
+# @param start_num(int)				starting post number
+# @param end_num(int)				ending post number
+# @return fixed_user_dict(dict)		(fixed user id) : (number of posts)
+# @return user_dict(dict)			(user name) : (number of posts)
+# @return fixed_user_names(dict)	(fixed user id) : [fixed user's names](list)
+#
+def collect_gallery_data_per_thread (gallery_id, start_num, end_num):
+	fixed_user_dict = dict ()
+	user_dict = dict ()
+	fixed_user_names = dict ()
+
+#
+# collect gallery's data.
+# @param gallery_id(str)			id of gallery
+# @param start_num(int)				starting post number
+# @param end_num(int)				ending post number
+# @return fixed_user_dict(dict)		(fixed user id) : (number of posts)
+# @return user_dict(dict)			(user name) : (number of posts)
+# @return fixed_user_names(dict)	(fixed user id) : [fixed user's names](list)
+#
+def collect_gallery_data (gallery_id, start_num, end_num):
+	# get number of posts per thread.
+	num_posts = end_num - start_num + 1
+	posts_per_thread = num_posts / NUM_THREADS
+	
+	# create threads.
+	thread_list = []
+	post_num = start_num
+	for i in range (NUM_THREADS - 1):
+		thread_list.append (ThreadWithReturn (target=collect_gallery_data_per_thread, args=(gallery_id, start_num, end_num)))
+
+#
 # main function.
 #
-def main():
+def main ():
 	gallery_id, start_num, end_num = get_gallery_info ()
+	fixed_user_dict, user_dict, fixed_user_names = collect_gallery_data (gallery_id, start_num, end_num)
+	print_result (user_dict)
 
 main()
